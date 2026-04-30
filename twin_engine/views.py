bb1fdab009fdc87085febc9918370969a52f5ff1
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-04-30 20:52:38 +0530

    Phase 1: integrated preference saving logic into twin_dashboard view

--- twin_engine/views.py
+++ twin_engine/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from .models import UserPreference, PastChoice
+from .forms import PreferenceForm
 from .logic import get_digital_twin_prediction
-from django.contrib.auth.decorators import login_required
 
-# Abhi ke liye hum login wala check hata rahe hain taaki aap asani se test kar sako
 def twin_dashboard(request):
+    pref, created = UserPreference.objects.get_or_create(user=request.user)
     prediction = None
+    
     if request.method == "POST":
-        user_scenario = request.POST.get('scenario')
-        
-        # Humne logic.py mein jo function banaya tha usse call kar rahe hain
-        # Dummy user bhej rahe hain testing ke liye
-        prediction = get_digital_twin_prediction(request.user, user_scenario)
+        if 'update_pref' in request.POST:
+            form = PreferenceForm(request.POST, instance=pref)
+            if form.is_valid():
+                form.save()
+        elif 'get_prediction' in request.POST:
+            scenario = request.POST.get('scenario')
+            prediction = get_digital_twin_prediction(request.user, scenario)
+            PastChoice.objects.create(user=request.user, scenario=scenario, choice_made=prediction)
 
-    return render(request, 'twin_dashboard.html', {'prediction': prediction})
+    form = PreferenceForm(instance=pref)
+    return render(request, 'twin_dashboard.html', {'prediction': prediction, 'form': form})
